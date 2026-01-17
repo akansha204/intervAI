7ad1fe680637493b1b3bdd0af4bcc6145c6f0950
Anmol Singh <anmolsingh7442@gmail.com>
2026-01-17 13:08:18 +0530

    feat: add interview Redux slice and session summary screen

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import interviewReducer from './slices/interviewSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
+        interview: interviewReducer,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
