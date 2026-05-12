e4aeee1a46aa10f9d2ef84e649347dac10e6859f
Anmol Singh <130905259+Anmolzezx@users.noreply.github.com>
2026-05-12 14:49:54 +0530

    fix: remove redux-persist from dependencies and update jest setup

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'react-native',
   setupFiles: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?@?react-native|@react-navigation|@react-native-async-storage|redux-persist|@reduxjs/toolkit|immer|react-redux)',
+    'node_modules/(?!(jest-)?@?react-native|@react-navigation|@react-native-async-storage|@reduxjs/toolkit|immer|react-redux)',
   ],
   testPathIgnorePatterns: ['/node_modules/', '/backend/'],
 };
--- jest.setup.js
+++ jest.setup.js
@@ -5,7 +5,62 @@ jest.mock('@react-native-async-storage/async-storage', () =>
 jest.mock('react-native-gesture-handler', () => ({}));
 
 jest.mock('react-native-reanimated', () => {
-  const Reanimated = require('react-native-reanimated/mock');
-  Reanimated.default.call = () => {};
-  return Reanimated;
+  const RN = require('react-native');
+
+  const makeChainable = () => {
+    const obj = {};
+    return new Proxy(obj, { get: () => () => obj });
+  };
+
+  const identity = (v) => v;
+
+  return {
+    __esModule: true,
+    default: {
+      View: RN.View,
+      createAnimatedComponent: (c) => c,
+      call: () => {},
+    },
+    View: RN.View,
+    Text: RN.Text,
+    ScrollView: RN.ScrollView,
+    Image: RN.Image,
+    createAnimatedComponent: (c) => c,
+    useSharedValue: (v) => ({ value: v }),
+    useAnimatedStyle: () => ({}),
+    useAnimatedProps: () => ({}),
+    useDerivedValue: () => ({ value: 0 }),
+    useAnimatedReaction: () => {},
+    withTiming: identity,
+    withSpring: identity,
+    withDecay: identity,
+    withDelay: (_, v) => v,
+    withSequence: (...vals) => vals[vals.length - 1],
+    withRepeat: identity,
+    cancelAnimation: () => {},
+    runOnJS: (fn) => fn,
+    runOnUI: (fn) => fn,
+    Easing: {
+      linear: identity,
+      ease: identity,
+      cubic: identity,
+      sin: identity,
+      out: () => identity,
+      in: () => identity,
+      inOut: () => identity,
+      bezier: () => identity,
+    },
+    FadeIn: makeChainable(),
+    FadeOut: makeChainable(),
+    FadeInUp: makeChainable(),
+    FadeInDown: makeChainable(),
+    FadeInLeft: makeChainable(),
+    FadeInRight: makeChainable(),
+    SlideInUp: makeChainable(),
+    SlideInDown: makeChainable(),
+    SlideInLeft: makeChainable(),
+    SlideInRight: makeChainable(),
+    ZoomIn: makeChainable(),
+    Layout: makeChainable(),
+  };
 });
