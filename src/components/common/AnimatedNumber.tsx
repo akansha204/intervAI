020f8bab79f979211b31a956b3eff2d4057acdd3
Anmol Singh <130905259+Anmolzezx@users.noreply.github.com>
2026-05-12 14:49:35 +0530

    feat: add AnimatedNumber and AnimatedProgressBar components

---
+++ src/components/common/AnimatedNumber.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import { Text, TextStyle, StyleProp } from 'react-native';
+import {
+    useSharedValue,
+    useDerivedValue,
+    withTiming,
+    Easing,
+    runOnJS,
+} from 'react-native-reanimated';
+
+type Props = {
+    value: number;
+    decimals?: number;
+    duration?: number;
+    prefix?: string;
+    suffix?: string;
+    style?: StyleProp<TextStyle>;
+};
+
+const AnimatedNumber: React.FC<Props> = ({
+    value,
+    decimals = 1,
+    duration = 1500,
+    prefix = '',
+    suffix = '',
+    style,
+}) => {
+    const sv = useSharedValue(0);
+    const [display, setDisplay] = useState('0');
+
+    useEffect(() => {
+        sv.value = 0;
+        sv.value = withTiming(value, { duration, easing: Easing.out(Easing.cubic) });
+    }, [value, duration, sv]);
+
+    useDerivedValue(() => {
+        runOnJS(setDisplay)(sv.value.toFixed(decimals));
+    }, [decimals]);
+
+    return (
+        <Text style={style}>
+            {prefix}
+            {display}
+            {suffix}
+        </Text>
+    );
+};
+
+export default AnimatedNumber;
